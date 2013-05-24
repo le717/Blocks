83f9b9fa8f690df71b99091ea877e2f4204f2b80
le717 <le717@e.mail>
2013-05-24 14:15:24 -0400

    Fix compile location

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ if len(sys.argv) == 1: sys.argv[1:] = ["build"]
 # Compile into the proper folder depending on the architecture
 # Based on code from the Python help file (platform module) and my own tests
 if sys.maxsize == 2147483647:
-    destfolder = "Compile2/Windows32"
+    destfolder = "Compile/Windows32"
 else:
-    destfolder = "Compile2/Windows64"
+    destfolder = "Compile/Windows64"
 
 build_exe_options = {"build_exe": destfolder,
                      "icon": "Media/Blocks.ico",
