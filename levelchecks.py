le717/Blocks

commit 2a745024c4e7c00130326e485f834d2a89b14009

Fix multiple regressions in file writer and syntax check:
* One too many trailing new lines written to file
* Removing last line in files that do not have trailing new lines (IXS
does have files like that)
* Incorrectly reporting syntax errors in files lacking trailing new
lines

Also:
* Fix some ReferenceErrors
* New parameter for _syntaxChecks(): levelLayout
* _syntaxChecks() returns layout suitable for writing if all checks
passed

levelchecks.py
@@ -36,8 +36,8 @@ class LevelChecks(object):
 
         @param layout {string} The level layout to be checked.
         """
-        self.__layoutNormCase = layout
-        self.__layout = layout.upper()
+        self.__layoutNormCase = layout.rstrip()
+        self.__layout = layout.rstrip().upper()
 
     def _levelSize(self):
         """Check the size of the layout."""
@@ -76,7 +76,7 @@ The level must be exactly 8 lines.""".format(numOfLines))
     def _lineLength(self):
         """Check the length of each line."""
         # Get the each line's number text, and length
-        for lineNum, lineText in enumerate(self.__layoutNormCase[:-1].split("\n")):
+        for lineNum, lineText in enumerate(self.__layoutNormCase.split("\n")):
             lineNum += 1
             lineLength = len(lineText)
 

Blocks.pyw
@@ -137,13 +137,13 @@ class Blocks(object):
         return True
 
     def _writeFile(self, filePath, fileName, firstLine,
-                   layout, temporary=False):
+                   levelLayout, temporary=False):
         """Write the level layout to file.
 
         @param filePath {string} Absolute path to the resulting file.
         @param fileName {string} File name for the resulting file.
         @param firstLine {bytes} The first line for the file.
-        @param layout {bytes} The level layout to be written.
+        @param levelLayout {bytes} The level layout to be written.
         @param temporary {boolean} If set to True, a temporary file will be
             created at "~".
         @return {boolean|string} True if temporary is set to False;
@@ -159,7 +159,7 @@ class Blocks(object):
             # First line, layout, file ending
             with open(os.path.join(filePath, fileName), "wb") as f:
                 f.write(firstLine)
-                f.write(layout)
+                f.write(levelLayout)
                 f.write(b"\r\n")
 
             if temporary:
@@ -170,7 +170,7 @@ class Blocks(object):
         except PermissionError as p:
             self._displayError("Insufficient Access Rights!",
                                "Blocks does not have the access rights to save {0}!"
-                               .format(levelFileName), p)
+                               .format(fileName), p)
             return False
 
     def _createBackup(self, location, backupFile):
@@ -195,22 +195,25 @@ class Blocks(object):
         except PermissionError as p:
             self._displayError("Insufficient Access Rights!",
                                "Blocks does not have the access rights to save {0}!"
-                               .format(levelFileName), p)
+                               .format(backupFile), p)
             return False
 
-    def _syntaxChecks(self):
+    def _syntaxChecks(self, levelLayout):
         """Check the level layout for syntax errors.
 
+        @param levelLayout {bytes} The level layout to be written.
         @return {boolean} False of a syntax error was found;
-            True otherwise.
+            level layout suitable for saving.
         """
-        results = levelchecks.LevelChecks(self.__levelLayout).checkLevel()
+        results = levelchecks.LevelChecks(levelLayout).checkLevel()
 
         # An error in the level was found, display the details
         if type(results) == tuple:
+            if const.debugMode:
+                print(results[0], results[1])
             self._displayError(results[0], results[1])
             return False
-        return True
+        return results
 
     def _selectDestFile(self):
         """File selection dialog for new level file.
@@ -245,7 +248,7 @@ class Blocks(object):
  F  F  F  F  F  F  F  F  F  F  F  F  F
  F  F  F  F  F  F  F  F  F  F  F  F  F
  F  F  F  F  F  F  F  F  F  F  F  F  F
- F  F  F  F  F  F  F  F  F  F  F  F  F\n\n"""
+ F  F  F  F  F  F  F  F  F  F  F  F  F\n"""
 
         if const.debugMode:
             print("\nA new level is being created.")
@@ -285,25 +288,27 @@ class Blocks(object):
         @return {boolean} False if any errors occurred;
             True otherwise.
         """
-        # Get new layout from text box, store it for possible further editing
-        levelLayout = gui.levelArea.get("1.0", "end -2 lines")
-        self.__levelLayout = levelLayout
+        # Get new layout from text box
+        levelLayout = gui.levelArea.get("1.0", "end")
 
         # We need to alias these in case a new file is being written
         filePath = self.__filePath
         fileName = self.__fileName
         firstLine = self.__firstLine
 
+        # Check the level layout for errors
+        levelLayout = self._syntaxChecks(levelLayout)
+
         # TODO Exception handling
 
         # The syntax checks failed
-        if not self._syntaxChecks():
+        if not levelLayout:
             return False
 
         # The syntax checks passed
         else:
             # Create a bytes version of the layout for accurate writing
-            binaryLayout = str.encode(self.__levelLayout, encoding="utf-8",
+            binaryLayout = str.encode(levelLayout, encoding="utf-8",
                                       errors="strict")
 
             # We are saving an existing file, make a backup first
@@ -324,6 +329,10 @@ class Blocks(object):
             if self._writeFile(filePath, fileName, firstLine, binaryLayout):
                 messagebox.showinfo("Success!", "Successfully saved {0} to {1}"
                                     .format(fileName, filePath))
+
+            # Could not save the file
+            else:
+                return False
 
             # Since we just saved a new level, we now need to load it
             if self.__newLevel:

commit 7d51fbfa59ecd0d1099f989dc85ee0e51d06f0b7

Make invalid character check case-insensitive again
Regression introduced in 2a745024c4e7c00130326e485f834d2a89b14009

levelchecks.py
@@ -37,8 +37,6 @@ class LevelChecks(object):
 
         @param layout {String} The level layout to be checked.
         """
-        # TODO What on Galidor happened here???
-        self.__layoutNormCase = layout.rstrip()
         self.__layout = layout.rstrip().upper()
 
     def _levelSize(self):
@@ -52,7 +50,7 @@ class LevelChecks(object):
             while second index and third index are both None.
         """
         # The level is not 8 lines
-        numOfLines = len(self.__layoutNormCase[:-2].split("\n"))
+        numOfLines = len(self.__layout[:-2].split("\n"))
         if (numOfLines != 8):
             return (True, "Size Error!", """Your level contains {0} lines!
 The level must be exactly 8 lines.""".format(numOfLines))
@@ -77,7 +75,7 @@ The level must be exactly 8 lines.""".format(numOfLines))
                     "WI", "WJ", "WM", "WL", "WR", "WT", "WV")
 
         # Get each each character's index and line location
-        for lineNum, lineText in enumerate(self.__layoutNormCase.split("\n")):
+        for lineNum, lineText in enumerate(self.__layout.split("\n")):
             lineNum += 1
             for charLoc, char in enumerate(lineText.split()):
                 charLoc += 1
@@ -103,7 +101,7 @@ The level must be exactly 8 lines.""".format(numOfLines))
             while second index and third index are both None.
         """
         # Get the each line's length and text
-        for lineNum, lineText in enumerate(self.__layoutNormCase.split("\n")):
+        for lineNum, lineText in enumerate(self.__layout.split("\n")):
             lineNum += 1
             lineLength = len(lineText)
 